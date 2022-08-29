debb0986bc5fced83ad3122731af68c65a7f4c4d
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-08-29 20:36:11 -0300

    tp pedidos asincronicos

---
+++ frontend/js/formulario.js
@@ -0,0 +1,62 @@
+const $ = (element) => document.getElementById(element)
+const date = moment()
+
+window.onload = async () => {
+    console.log('formulario.js success');
+    let query = new URLSearchParams(location.search)
+
+    if(query.has('id')){
+
+        $('botonEditar').hidden = false
+
+        try {
+
+            let response = await fetch(`${sessionStorage.getItem('urlBase')}/api/movies/${query.get('id')}`)
+            let result = await response.json()
+            let movie = result.data
+
+            $('title').value = movie.title
+            $('rating').value = movie.rating
+            $('awards').value = movie.awards
+            $('release_date').value = moment(movie.release_date).format('YYYY-MM-DD')
+            $('length').value = movie.length
+            
+        } catch (error) {
+            console.error;
+        }
+
+        $('formulario').addEventListener('submit', async (e) =>{
+            e.preventDefault()
+            console.log('enviando formulario...');
+
+            let bodyForm = {
+                title: $('title').value,
+                rating: $('rating').value,
+                awards: $('awards').value,
+                release_date: $('release_date').value,
+                length: $('length').value,
+            }
+
+            try {
+
+                let response = await fetch(`${sessionStorage.getItem
+                    ('urlBase')}/api/movies/update/${query.get('id')}`,{
+                    method: 'PUT',
+                    body: JSON.stringify(bodyForm),
+                    headers: {
+                        'content-Type' : 'application/json'
+                    },
+                })
+                console.log(response)
+                let result = await response.json()
+                console.log(result);
+
+            } catch (error) {
+                console.error
+            }
+        })
+
+    }else{
+        $('botonAgregar').hidden = false
+    }
+}
